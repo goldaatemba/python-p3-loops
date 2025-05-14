ab610086355fc157efdb93aa5ef815df1dbb1567
Golda Atemba <goldaatemba610@gmail.com>
2025-05-14 07:39:43 +0300

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,62 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!") # happy_new_year()
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+
+
+def square_integers(numbers):
+    squared = []
+    for num in numbers:
+        squared.append(num ** 2)
+    return squared
+# print(square_integers([1, 2, 3, 4, 5]))  # Should output [1, 4, 9, 16, 25]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i) # fizzbuzz()
+
+def happy_new_year():
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+
+
+def square_integers(numbers):
+    squared = []
+    for num in numbers:
+        squared.append(num ** 2)
+    return squared
+
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+# Uncomment the lines below to test
+# happy_new_year()
+# print(square_integers([1, 2, 3, 4, 5]))
+# fizzbuzz()
+
